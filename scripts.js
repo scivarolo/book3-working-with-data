21aae0b552575f258c457af8a2b17c165b9c3816
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-15 13:38:32 -0500

    Log how many times each event type occurs.

--- scripts.js
+++ scripts.js
@@ -16,7 +16,42 @@ for (let i = 0; i < githubData.length; i++) {
 
 console.log("Total Commits:", commitCount);
 
+// -----------------------------------------------------------------------------
+
 // How many of each event type are there? (PullRequestEvent, PushEvent, etc)
+
+  // Make array to store event types.
+  let eventTypes = [];
+  
+  // Query event type for each object.
+  for (let i = 0; i < githubData.length; i++) {
+    let eventType = githubData[i].type;
+
+    // If event type is not in array, add it. If not, move on.
+    if(eventTypes.indexOf(eventType) === -1) {
+      eventTypes.push(eventType);
+    }
+  }
+
+  // Create an object to store the count of each event type.
+  let eventTypesCount = {};
+  for (let i = 0; i < eventTypes.length; i++) {
+    eventTypesCount[eventTypes[i]] = 0;
+  }
+
+  // Loop through events again and increase counter object depending on event type.
+
+  for (let i = 0; i < githubData.length; i++) {
+    let eventType = githubData[i].type;
+    eventTypesCount[eventType] += 1;
+  }
+  
+  // Print results to console.
+  for(eventType in eventTypesCount) {
+    console.log(`${eventType} occurred ${eventTypesCount[eventType]} times.`);
+  }
+  
+//-------------------------------------------------------------------------------  
 
 // List all Github users who submitted a pull request that was approved by Steve.
 
