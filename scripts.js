0caa331fb358c5063bda82378115485c308f8425
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-15 14:07:59 -0500

    Find all users who had Pull Request approved (closed) by Steve

--- scripts.js
+++ scripts.js
@@ -50,10 +50,41 @@ console.log("Total Commits:", commitCount);
   for(eventType in eventTypesCount) {
     console.log(`${eventType} occurred ${eventTypesCount[eventType]} times.`);
   }
-  
+
 //-------------------------------------------------------------------------------  
 
 // List all Github users who submitted a pull request that was approved by Steve.
+
+  // Make array of all pull requests.
+  // let pullRequests = [];
+
+  // Make array to hold users that submitted pull requests approved by Steve
+  let prUsers = [];
+
+  // Loop through events and do something if type is PullRequestEvent
+  for (let i = 0; i < githubData.length; i++) {
+    let event = githubData[i];
+    if(event.type === "PullRequestEvent") {
+      // Get user that created Pull Request
+      let user = event.payload.pull_request.user.login;
+      
+      // Store PullRequestEvent Action (open or closed)
+      let action = event.payload.action;
+
+      // check if PR was approved by stevebrownlee
+      // Excludes any "open" events
+      if (action === "closed") {
+        // Add user to prUsers array if not in there already.
+        if (prUsers.indexOf(user) === -1) {
+          prUsers.push(user);
+        }    
+      }
+    }
+  }
+
+  console.log(`The following users have submitted Pull Requests approved by Steve: ${prUsers.join(", ")}.`);
+
+//-------------------------------------------------------------------------------
 
 // List all repositories on which Steve had an event, and show how many events were on each one.
 
