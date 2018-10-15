4c8d7435e725ac0117d2dfac7f170dd8d7f9577b
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-15 11:49:18 -0500

    Starter files with github data

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// How many total commits were made in all of Steve's events?
+
+// How many of each event type are there? (PullRequestEvent, PushEvent, etc)
+
+// List all Github users who submitted a pull request that was approved by Steve.
+
+// List all repositories on which Steve had an event, and show how many events were on each one.
+
+// Which event had the most number of commits?
+
+// Which programming langugages were affected by Steve's events?
+
+// What programming language was the most affected by Steve's events?
