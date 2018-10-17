884608ae4f797d5cea213dc8f41bfafbf699c02c
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-17 11:29:41 -0500

    Refactor '2. how many of each event' with compare statement

--- scripts.js
+++ scripts.js
@@ -14,37 +14,25 @@ console.log("Total Commits:", commitCount);
 
 // -----------------------------------------------------------------------------
 
-// How many of each event type are there? (PullRequestEvent, PushEvent, etc)
+// 2. How many of each event type are there? (PullRequestEvent, PushEvent, etc)
 
   // Make array to store event types.
-  let eventTypes = [];
+  let allEventTypes = [];
   
-  // Query event type for each object.
-  for (let i = 0; i < githubData.length; i++) {
-    let eventType = githubData[i].type;
+  githubData.forEach((event) => {
+    allEventTypes.push(event.type);
+  });
 
-    // If event type is not in array, add it. If not, move on.
-    if(eventTypes.indexOf(eventType) === -1) {
-      eventTypes.push(eventType);
-    }
-  }
+  //Make object to count each eventtype in array
+  let eventTypesCounter = {};
+  allEventTypes.forEach((eventType) => {
+    //If eventType is already in object, 
+    eventTypesCounter[eventType] = (eventTypesCounter[eventType] || 0) + 1;
+  });
 
-  // Create an object to store the count of each event type.
-  let eventTypesCount = {};
-  for (let i = 0; i < eventTypes.length; i++) {
-    eventTypesCount[eventTypes[i]] = 0;
-  }
-
-  // Loop through events again and increase counter object depending on event type.
-
-  for (let i = 0; i < githubData.length; i++) {
-    let eventType = githubData[i].type;
-    eventTypesCount[eventType] += 1;
-  }
-  
   // Print results to console.
-  for(eventType in eventTypesCount) {
-    console.log(`${eventType} occurred ${eventTypesCount[eventType]} times.`);
+  for(eventType in eventTypesCounter) {
+    console.log(`${eventType} occurred ${eventTypesCounter[eventType]} times.`);
   }
 
 //-------------------------------------------------------------------------------  
